783c5e645d70469a5fbd414a9524c6f8c1ca8244
emilyewesel <ewesel@stanford.edu>
2024-01-26 00:25:18 -0800

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
 df = pd.read_csv('/dataNAS/people/paschali/datasets/chexpert-public/chexpert-public/train.csv')
 print(df.head())
 df.dropna(subset=['Support Devices'], inplace=True)
-
+baase = "/dataNAS/people/paschali/datasets/chexpert-public/chexpert-public/"
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -122,6 +122,7 @@ def load_and_preprocess_image(img_path):
 # Extract features from images
 X_images = []
 for img_path in X['Path']:
+    img_path = baase + img_path
     img_array = load_and_preprocess_image(img_path)
     features = vgg16_model.predict(img_array)
     X_images.append(features.flatten())
